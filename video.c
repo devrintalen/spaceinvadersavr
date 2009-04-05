0296eda401792824b2e9cf99a7bbd7bc0ddfd2ce
Devrin Talen <devrin.talen@gmail.com>
2009-04-05 14:57:41 -0400

    Basic drawing code is implemented. Needs to be tested.

--- video.c
+++ video.c
@@ -15,6 +15,8 @@
 
 #define WIDTH 144
 #define HEIGHT 200
+// buffer size = (width / 8) * height = 144/8 * 200 = 3600
+#define FRAMEBUFFERSIZE 3600
 
 #define TOPLINE 30
 #define BOTLINE 230
@@ -28,13 +30,13 @@
 #define V_SYNC 0x1
 #define V_NOSYNC 0x0
 
-uint8_t framebuffer[WIDTH][HEIGHT];
-uint8_t pos;
+uint8_t framebuffer[FRAMEBUFFERSIZE];
+uint8_t *pos;
 uint16_t line;
 
 uint8_t init(void)
 {
-    uint8_t i,j;
+    uint16_t i;
 
     // Set up timer0 to the sync frequency (63.5us)
     // 63.5E-6 / (1/16E6) = 1016 = (127 * 8)
@@ -45,12 +47,9 @@ uint8_t init(void)
     TIMSK = 0x0;
 
     // Initialize the framebuffer
-    for (i=0; i<WIDTH; i++)
+    for (i=0; i<FRAMEBUFFERSIZE; i++)
     {
-        for (j=0; j<HEIGHT; j++)
-        {
-            framebuffer[i][j] = 0;
-        }
+        framebuffer[i] = 0;
     }
 
     return 1;
@@ -75,18 +74,19 @@ uint8_t set_pixel(uint8_t x, uint8_t y)
         return 0;
     }
 
+    // TODO this is broken
     framebuffer[x][y] = 1;
     
     return 1;
 }
 
-// vertical sync interrupt
+// Sync interrupt _must_ be entered from sleep mode.
 ISR(TIMER0_COMP_vect)
 {
     // generate sync pulse (5us)
-    VIDEO = (line < V_SYNC_START) ? V_SYNC : H_SYNC;
+    VIDEO = (line < V_SYNC_START) ? H_SYNC : V_SYNC;
     _delay_us(5);
-    VIDEO = (line < V_SYNC_START) ? V_NOSYNC : H_NOSYNC;
+    VIDEO = (line < V_SYNC_START) ? H_NOSYNC : V_NOSYNC;
 
     // start a new frame after line 262
     if (line == BOTLINE)
@@ -95,8 +95,139 @@ ISR(TIMER0_COMP_vect)
     // blast line buffer to screen
     if ((line > TOPLINE) && (line < BOTLINE))
     {
-        // TODO
+        // position = base + (line * (width/8))
+        // 144 / 8 = (2^7 + 2^4) / 2^3 = 2^4 + 2^1
+        pos = framebuffer + ((line - TOPLINE) << 4) + ((line - TOPLINE) << 1);
+        _delay_us(12);
+
+        // draw line
+        byteblast();
     }
 
     line++;
 }
+
+
+// The following code is copied from the ECE476 website at
+// http://instruct1.cit.cornell.edu/courses/ee476/video/videoGCC/video_144x200_GCC644.c
+// Copyright 2009 Shane Pryor and Bruce Land
+asm(".MACRO videobits\n\t"
+
+    "BST  R4,7\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,6\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,5\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,4\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,3\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,2\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,1\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    //"NOP \n\t" 
+    "NOP \n\t"
+    "BST  R4,0\n\t"
+    "BLD  R30,5\n\t"
+    "OUT  0x0b,R30\n\t"
+
+    ".ENDM\n\t"
+);
+
+// puts 18 bytes (1 line) to the screen
+void byteblast() {
+    asm("LDS  R26,pos\n\t"
+        "LDS  R27,pos+1\n\t"
+            
+        "LDI R30, 0x40\n\t"		
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X+\n\t"
+        "videobits\n\t"
+
+        "LD  R4,X\n\t"
+        "videobits\n\t"
+
+        "CLT\n\t"
+        "LDI R30, 0x40\n\t"
+
+        "BLD  R30,5\n\t"
+        "OUT  0x0b,R30\n\t"	
+    );
+}
+
