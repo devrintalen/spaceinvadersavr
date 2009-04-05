e5e6132d3845474f16d5346a41b648963751a8cb
Devrin Talen <devrin.talen@gmail.com>
2009-04-05 14:18:21 -0400

    Moved to standard int types.

--- video.h
+++ video.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 
-bool init(void);
-bool enable(void);
-bool disable(void);
+uint8_t init(void);
+uint8_t enable(void);
+uint8_t disable(void);
 
-bool set_pixel(uint8_t x, uint8_t y);
+uint8_t set_pixel(uint8_t x, uint8_t y);
