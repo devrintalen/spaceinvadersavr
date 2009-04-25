643d5a191b7bf25b60dc7ac0eae27078c85d87da
Devrin Talen <devrin.talen@gmail.com>
2009-04-25 14:37:09 -0400

    Time to go to a museum. Working on sprite methods.

--- engine.c
+++ engine.c
@@ -14,6 +14,7 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 
 #include <stdint.h>
+#include <assert.h>
 
 /* Sprite data structure.
  *
@@ -28,10 +29,60 @@ struct {
 
 struct {
     struct sprite *s;
-    struct list_el *next;
-} list_el;
+    struct node *next;
+} node;
     
-struct list_el sprite_hash[256];
+struct node sprite_hash[256];
+
+sprite_t create_sprite(uint8_t x, uint8_t y, uint8_t tile[])
+{
+    struct sprite sp;
+    struct node n;
+    struct node iter;
+   
+    sp = malloc(sizeof(sprite));
+    n = malloc(sizeof(node));
+    if (!sp || !n) {
+        return NULL;
+    }
+
+    sp.x = x;
+    sp.y = y;
+    sp.tile = tile;
+
+    n.s = &sp;
+    n.next = NULL;
+
+    iter = sprite_hash[sp.y];
+    while (iter->next) {
+        iter = iter->next;
+    }
+    iter.next = &n;
+
+    return &sp;
+}
+
+void remove_sprite(sprite_t s);
+{
+    struct node *iter;
+
+    assert(s);
+    assert(sprite_hash[s.y]);
+
+    iter = &sprite_hash[s.y];
+    if (iter->s == s)
+    {
+    }
+    else
+    {
+        while ((iter->next) && (iter->s != s))
+        {
+            iter = iter->next;
+        }
+        assert(iter->s == s);
+
+
+}
 
 /* Returns uint8_t[32] for a total of 256 horizontal px. Bytes are to be
  * rendered left to right from render[31].msb -> render[0].lsb.
@@ -41,7 +92,7 @@ uint8_t* render_line(uint8_t line)
     uint8_t start_line;
     uint8_t render[32]; // 256 horizontal px
     uint8_t bits;
-    struct list_el l;
+    struct node n;
     struct sprite sp;
 
     // Sprites to draw may be as far as 7 lines above
@@ -49,10 +100,10 @@ uint8_t* render_line(uint8_t line)
 
     for (i = start_line; i <= line; i++)
     {
-        l = sprite_hash[i];
-        while (l)
+        n = sprite_hash[i];
+        while (n)
         {
-            sp = l->s;
+            sp = n->s;
             bits = sp.tile[line - i];
             left_chunk = 31 - (sp.x / 8);
             right_chunk = left_chunk - 1;
@@ -68,7 +119,7 @@ uint8_t* render_line(uint8_t line)
                 render[right_chunk] |= bits << (8 - (sp.x % 8));
             }
 
-            l = l->next;
+            n = n->next;
         }
     }
 
